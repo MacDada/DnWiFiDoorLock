34e16b863874cb56e366c6492b85580a1a918557
MacDada <code@dnowak.pl>
2022-12-03 23:42:04 +0100

    debugging WiP

--- src/native.cpp
+++ src/native.cpp
@@ -1,4 +1,14 @@
 // nothing to do here, we just need this file for the `native` env to be built
+#include <stdio.h>
+
 int main() {
+    int foo = 4;
+
+    int bar = 2;
+
+    int baz = foo - bar;
+
+    printf("\n\nHello world! %d\n\n", baz);
+
     return 0;
 }
