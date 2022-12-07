562a5a7bd177acc9001be24a844ebeb8893e974f
MacDada <code@dnowak.pl>
2022-12-07 05:59:12 +0100

    asserts: typo

--- lib/DnAppUnity/src/DnApp/Unity/asserts.h
+++ lib/DnAppUnity/src/DnApp/Unity/asserts.h
@@ -11,9 +11,9 @@
         "Expected `" #actualObject "` to be an instance of `" #expectedClass "`" \
     )
 
-#define TEST_ASSERT_NOT_INSTANCE_OF(expectedClass, actualObject)                  \
-    UNITY_TEST_ASSERT(                                                            \
-        !(DnApp::Unity::instance_of<expectedClass>(actualObject)),                \
-        __LINE__,                                                                 \
-        "Expected `" #actualObject "` not to an instance of `" #expectedClass "`" \
+#define TEST_ASSERT_NOT_INSTANCE_OF(expectedClass, actualObject)                     \
+    UNITY_TEST_ASSERT(                                                               \
+        !(DnApp::Unity::instance_of<expectedClass>(actualObject)),                   \
+        __LINE__,                                                                    \
+        "Expected `" #actualObject "` not to be an instance of `" #expectedClass "`" \
     )
