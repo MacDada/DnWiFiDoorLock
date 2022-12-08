3b62956f559f1f3e271e3fc379ccbf9cb30e9e66
MacDada <code@dnowak.pl>
2022-12-08 06:20:25 +0100

    NullLoggerTest.cpp: less repetition with a macro

--- test/native/DnLogger/test_NullLogger/NullLoggerTest.cpp
+++ test/native/DnLogger/test_NullLogger/NullLoggerTest.cpp
@@ -6,6 +6,21 @@
 #include "DnApp/Logger/NullLogger.h"
 #include "DnApp/Unity/asserts.h"
 
+#define DN_APP_LOGGER_NULL_LOGGER_TEST_ALL_LOG_METHODS_AND_LEVELS(message) { \
+    logger.log(Logger::LOG_LEVEL::DEBUG, message); \
+    logger.log(Logger::LOG_LEVEL::DEBUG, message); \
+    logger.log(Logger::LOG_LEVEL::INFO, message); \
+    logger.log(Logger::LOG_LEVEL::WARNING, message); \
+    logger.log(Logger::LOG_LEVEL::ERROR, message); \
+    logger.log(Logger::LOG_LEVEL::CRITICAL, message); \
+    \
+    logger.debug(message); \
+    logger.info(message); \
+    logger.warning(message); \
+    logger.error(message); \
+    logger.critical(message); \
+}
+
 namespace {
     using DnApp::Logger::Logger;
     using DnApp::Logger::NullLogger;
@@ -19,51 +34,19 @@ namespace {
     void test_logging_chars() {
         char foo[4] = "foo";
 
-        logger.log(Logger::LOG_LEVEL::DEBUG, foo);
-        logger.log(Logger::LOG_LEVEL::INFO, foo);
-        logger.log(Logger::LOG_LEVEL::WARNING, foo);
-        logger.log(Logger::LOG_LEVEL::ERROR, foo);
-        logger.log(Logger::LOG_LEVEL::CRITICAL, foo);
-
-        logger.debug(foo);
-        logger.info(foo);
-        logger.warning(foo);
-        logger.error(foo);
-        logger.critical(foo);
+        DN_APP_LOGGER_NULL_LOGGER_TEST_ALL_LOG_METHODS_AND_LEVELS(foo);
     }
 
     void test_logging_const_chars() {
         const char foo[4] = "foo";
 
-        logger.log(Logger::LOG_LEVEL::DEBUG, foo);
-        logger.log(Logger::LOG_LEVEL::INFO, foo);
-        logger.log(Logger::LOG_LEVEL::WARNING, foo);
-        logger.log(Logger::LOG_LEVEL::ERROR, foo);
-        logger.log(Logger::LOG_LEVEL::CRITICAL, foo);
-
-        logger.debug(foo);
-        logger.info(foo);
-        logger.warning(foo);
-        logger.error(foo);
-        logger.critical(foo);
-    }
-
-    auto make_unique_ptr_of_chars() {
-        return std::make_unique<char[]>(4);
+        DN_APP_LOGGER_NULL_LOGGER_TEST_ALL_LOG_METHODS_AND_LEVELS(foo);
     }
 
     void test_logging_unique_ptr_of_chars() {
-        logger.log(Logger::LOG_LEVEL::DEBUG, make_unique_ptr_of_chars());
-        logger.log(Logger::LOG_LEVEL::INFO, make_unique_ptr_of_chars());
-        logger.log(Logger::LOG_LEVEL::WARNING, make_unique_ptr_of_chars());
-        logger.log(Logger::LOG_LEVEL::ERROR, make_unique_ptr_of_chars());
-        logger.log(Logger::LOG_LEVEL::CRITICAL, make_unique_ptr_of_chars());
+        auto foo = std::make_unique<char[]>(4);
 
-        logger.debug(make_unique_ptr_of_chars());
-        logger.info(make_unique_ptr_of_chars());
-        logger.warning(make_unique_ptr_of_chars());
-        logger.error(make_unique_ptr_of_chars());
-        logger.critical(make_unique_ptr_of_chars());
+        DN_APP_LOGGER_NULL_LOGGER_TEST_ALL_LOG_METHODS_AND_LEVELS(std::make_unique<char[]>(4));
     }
 }
 
