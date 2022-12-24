23b8ec186b5370fa8d45da4a2db17cc19197ee06
MacDada <code@dnowak.pl>
2022-12-24 19:10:04 +0100

    added HardwareSerialLogger.h

---
+++ lib/DnAppArduino/src/DnApp/Arduino/Logger/Endpoint/HardwareSerialLogger.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <type_traits>
+
+#include <Arduino.h>
+
+#include "DnApp/Logger/Endpoint/PrinterLogger.h"
+
+namespace DnApp::Arduino::Logger::Endpoint {
+    using HardwareSerialLogger = DnApp::Logger::Endpoint::PrinterLogger<HardwareSerial>;
+
+    static_assert(!std::is_abstract<HardwareSerialLogger>());
+}
--- lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Logger/HardwareSerialLogger.h
+++ lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Logger/HardwareSerialLogger.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <type_traits>
+
 #include <Arduino.h>
 
 #include "DnWiFiDoorLock/Arduino/Logger/PrintLnLogger.h"
