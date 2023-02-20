c2bda3e2ea208f8243bd329575b90f218f8ae607
MacDada <code@dnowak.pl>
2023-02-20 01:13:28 +0100

    native.cpp: printing compiler info, nothing better to do anyway ;p

--- src/native.cpp
+++ src/native.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+
 // nothing to do here, we just need this file for the `native` env to be built
 int main() {
+    std::cout << __cplusplus << std::endl;
+
+#ifdef __clang__
+    std::cout << "CLANG" << std::endl;
+#else
+    std::cout << "GCC" << std::endl;
+#endif
+
+#ifdef __clang__
+    std::cout
+        << __clang_major__
+        << "."
+        << __clang_minor__
+        << "."
+        << __clang_patchlevel__
+        << std::endl;
+#else
+    std::cout
+        << __GNUC__
+        << "."
+        << __GNUC_MINOR__
+        << "."
+        << __GNUC_PATCHLEVEL__
+        << std::endl;
+#endif
+
     return 0;
 }
