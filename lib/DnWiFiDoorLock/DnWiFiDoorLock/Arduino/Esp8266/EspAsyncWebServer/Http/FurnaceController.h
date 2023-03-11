MacDada/DnWiFiDoorLock

commit 894b03d95838f30e056df076c08303acb42bd12e

using PSTR() everywhere, F() is unnecessary -> at least on ESP-8266
this commit does not increase RAM usage -> as F() does the same as PSTR(), plus extra casting
actually, it saved a small portion of Flash -> not sure why ;p

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/FurnaceController.h
@@ -74,7 +74,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
 
             furnace.toggleHeater();
 
-            redirect(request, F("/furnace"));
+            redirect(request, PSTR("/furnace"));
         }
 
         void apiGetAction(AsyncWebServerRequest& request) const {
@@ -83,7 +83,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_JSON,
-                furnace.isHeaterOn() ? F("true") : F("false")
+                furnace.isHeaterOn() ? PSTR("true") : PSTR("false")
             );
         }
 
@@ -93,9 +93,9 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         ) const {
             logger.info(PSTR("FurnaceController::apiPostAction()"));
 
-            if (body.equals(F("ON"))) {
+            if (body.equals(PSTR("ON"))) {
                 furnace.turnOnHeater();
-            } else if (body.equals(F("OFF"))) {
+            } else if (body.equals(PSTR("OFF"))) {
                 furnace.turnOffHeater();
             } else {
                 logger.warning(PSTR(
@@ -105,7 +105,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
                 request.send(
                     HTTP_RESPONSE_STATUS_BAD_REQUEST,
                     HTTP_RESPONSE_CONTENT_TYPE_JSON,
-                    F("Invalid value")
+                    PSTR("Invalid value")
                 );
 
                 return;
@@ -114,7 +114,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             request.send(
                 HTTP_RESPONSE_STATUS_ACCEPTED,
                 HTTP_RESPONSE_CONTENT_TYPE_JSON,
-                furnace.isHeaterOn() ? F("true") : F("false")
+                furnace.isHeaterOn() ? PSTR("true") : PSTR("false")
             );
         }
     private:

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/Controller.h
@@ -52,7 +52,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             AsyncWebServerResponse* const response = request.beginResponse(
                 HTTP_RESPONSE_STATUS_REDIRECT
             );
-            response->addHeader(F("Location"), location);
+            response->addHeader(PSTR("Location"), location);
             request.send(response);
         }
     };

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/DoorLockController.h
@@ -75,7 +75,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             doorLock.switchOpenClose();
 
             // todo: route generator
-            redirect(request, F("/doorlock"));
+            redirect(request, PSTR("/doorlock"));
         }
     private:
         const DnWiFiDoorLock::Arduino::Hardware& hardware;

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/ServerSetup.h
@@ -82,7 +82,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
 
             request.send(
                 404,
-                F("text/plain"),
+                PSTR("text/plain"),
                 DnApp::Common::Strings::format(
                     PSTR("File Not Found\n\nURI: %s\nMethod: %s\n"),
                     request.url().c_str(),
@@ -93,7 +93,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
 
         void setupRouting() {
             server.on(PSTR("/"), HTTP_GET, [&](AsyncWebServerRequest* const request) {
-                request->redirect(F("/furnace"));
+                request->redirect(PSTR("/furnace"));
             });
 
             // https://discord.com/channels/583251190591258624/742849025191051326/995832013405835316
@@ -175,17 +175,17 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void logServerHasStarted() {
-            String message{F("HTTP server has started, open http://")};
+            String message{PSTR("HTTP server has started, open http://")};
 
             message += wiFi.localIP().toString();
 
-            message += F(":");
+            message += PSTR(":");
             message += port;
-            message += F(" or http://");
+            message += PSTR(" or http://");
             message += hostname;
-            message += F(".local:");
+            message += PSTR(".local:");
             message += port;
-            message += F(" in a web browser :)");
+            message += PSTR(" in a web browser :)");
 
             logger.info(message);
         }

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/ServoButtonController.h
@@ -31,9 +31,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         };
 
         void showSettingsAction(AsyncWebServerRequest& request) {
-            // todo: this is probably casting the literal to String object
-            //       - maybe not the best idea after all?
-            logger.info(F("ServoButtonController::showSettingsAction()"));
+            logger.info(PSTR("ServoButtonController::showSettingsAction()"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
@@ -43,7 +41,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void updateSettingsAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::updateSettingsAction()"));
+            logger.info(PSTR("ServoButtonController::updateSettingsAction()"));
 
             auto settingsOrError = validateForm(request);
 
@@ -64,13 +62,13 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         // todo: include file as a string literal?
         //       https://forum.arduino.cc/t/how-to-store-a-file-in-code-memory/1075563/5
         void cssAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::cssAction()"));
+            logger.info(PSTR("ServoButtonController::cssAction()"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_CSS,
                 // language=CSS
-                F(R"(
+                PSTR(R"(
                     body {
                         text-align: center;
                     }
@@ -105,13 +103,13 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void modalCssAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::modalCssAction()"));
+            logger.info(PSTR("ServoButtonController::modalCssAction()"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_CSS,
                 // language=CSS
-                F(R"(
+                PSTR(R"(
                     #modal-overlay {
                         position: fixed;
                         top: 0;
@@ -162,7 +160,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void modalJsAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::modalJsAction()"));
+            logger.info(PSTR("ServoButtonController::modalJsAction()"));
 
             // todo: extract to "statically" served file: LittleFS
             //       * https://arduino-esp8266.readthedocs.io/en/latest/filesystem.html
@@ -173,7 +171,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT,
                 // language=JavaScript
-                F(R"(
+                PSTR(R"(
                     const overlay = document.createElement('div');
                     overlay.id = 'modal-overlay';
 
@@ -230,13 +228,13 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void inputNumberButtonsJsAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::inputNumberButtonsJsAction()"));
+            logger.info(PSTR("ServoButtonController::inputNumberButtonsJsAction()"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT,
                 // language=JavaScript
-                F(R"(
+                PSTR(R"(
                     function addButtonsContainer(angleNumberInput) {
                         const buttonsContainer = document.createElement('div');
                         buttonsContainer.classList.add('buttons-container');
@@ -302,13 +300,13 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void ajaxFormJsAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::ajaxFormJsAction()"));
+            logger.info(PSTR("ServoButtonController::ajaxFormJsAction()"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT,
                 // language=JavaScript
-                F(R"(
+                PSTR(R"(
                     import {modal} from './modal.js';
 
                     export function ajaxForm(form, onResponseData) {
@@ -351,14 +349,14 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void javascriptAction(AsyncWebServerRequest& request) {
-            logger.info(F("ServoButtonController::javascriptAction()"));
+            logger.info(PSTR("ServoButtonController::javascriptAction()"));
 
             // todo: extract to "statically" served file? LittleFS
             request.send(
                 HTTP_RESPONSE_STATUS_OK,
                 HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT,
                 // language=JavaScript
-                F(R"(
+                PSTR(R"(
                     import { addButtons } from './input-number-buttons.js';
                     import { ajaxForm } from './ajax-form.js';
 
@@ -405,13 +403,13 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
 
         String renderSettingsFormPage() const {
             // todo: make PrependMessageDecoratorLogger to stop repeating class' name
-            logger.info(F(
+            logger.info(PSTR(
                 "ServoButtonController::renderSettingsFormPage(): generating pageContent"
             ));
 
             // todo: maybe extract SettingsView
             // language=HTML
-            String page = F(R"(
+            String page = PSTR(R"(
                 <!DOCTYPE html>
                 <html>
                     <head>
@@ -500,19 +498,19 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
                 </html>
             )");
 
-            page.replace(F("{{ app_name }}"), appName);
-            page.replace(F("{{ button_name }}"), F("Heater"));
-            page.replace(F("{{ servo_min_angle }}"), String(Servo::MIN_ANGLE));
-            page.replace(F("{{ servo_max_angle }}"), String(Servo::MAX_ANGLE));
-            page.replace(F("{{ pressing_angle }}"), String(button.getPressingAngle()));
-            page.replace(F("{{ not_pressing_angle }}"), String(button.getNotPressingAngle()));
-            page.replace(F("{{ pressing_milliseconds }}"), String(button.getPressingMilliseconds()));
+            page.replace(PSTR("{{ app_name }}"), appName);
+            page.replace(PSTR("{{ button_name }}"), PSTR("Heater"));
+            page.replace(PSTR("{{ servo_min_angle }}"), String(Servo::MIN_ANGLE));
+            page.replace(PSTR("{{ servo_max_angle }}"), String(Servo::MAX_ANGLE));
+            page.replace(PSTR("{{ pressing_angle }}"), String(button.getPressingAngle()));
+            page.replace(PSTR("{{ not_pressing_angle }}"), String(button.getNotPressingAngle()));
+            page.replace(PSTR("{{ pressing_milliseconds }}"), String(button.getPressingMilliseconds()));
 
             // todo: extract render() after all? ;-)
             // #include <tuple>
 //            for (auto const& [find, replace] : std::vector<std::tuple<const String, const String>>{
-//                {F("{{ app_name }}"), F("Furnace")},
-//                {F("{{ pressing_milliseconds }}"), String(button.getPressingMilliseconds())}
+//                {PSTR("{{ app_name }}"), PSTR("Furnace")},
+//                {PSTR("{{ pressing_milliseconds }}"), String(button.getPressingMilliseconds())}
 //            }) {
 //                page.replace(find, replace);
 //            }
@@ -529,7 +527,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
                 || !maybeNewNotPressingAngle
                 || !maybeNewMilliseconds
                 ) {
-                return F("No required data given");
+                return PSTR("No required data given");
             }
 
             int newPressingAngle = maybeNewPressingAngle->toInt();
@@ -537,15 +535,15 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             int newMilliseconds = maybeNewMilliseconds->toInt();
 
             if (!isValidAngle(newPressingAngle)) {
-                return F("Invalid pressing angle");
+                return PSTR("Invalid pressing angle");
             }
 
             if (!isValidAngle(newNotPressingAngle)) {
-                return F("Invalid not pressing angle");
+                return PSTR("Invalid not pressing angle");
             }
 
             if (newMilliseconds <= 0) {
-                return F("Invalid pressing milliseconds");
+                return PSTR("Invalid pressing milliseconds");
             }
 
             return Settings{newPressingAngle, newNotPressingAngle, newMilliseconds};
@@ -559,7 +557,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             button.setNotPressingAngle(settings.notPressingAngle);
             button.setPressingMilliseconds(settings.pressingMilliseconds);
 
-            logger.info(F("ServoButtonController: updated settings"));
+            logger.info(PSTR("ServoButtonController: updated settings"));
 
             request.send(
                 HTTP_RESPONSE_STATUS_OK,

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/ServoController.h
@@ -129,7 +129,7 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             // todo: for some reason, when I hit enter from the number input, two values are sent:
             //       zero and the input value -> causing the value to be zero after all
             //       it works ok when I click submit instead of hitting enter
-            String content = F(R"(
+            String content = PSTR(R"(
                 <!DOCTYPE html>
                 <html>
                     <head>
@@ -176,26 +176,26 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
                 </html>
             )");
 
-            content.replace(F("{{ app_name }}"), appName);
+            content.replace(PSTR("{{ app_name }}"), appName);
 
-            content.replace(F("{{ servo_min_angle }}"), String(Servo::MIN_ANGLE));
-            content.replace(F("{{ servo_max_angle }}"), String(Servo::MAX_ANGLE));
+            content.replace(PSTR("{{ servo_min_angle }}"), String(Servo::MIN_ANGLE));
+            content.replace(PSTR("{{ servo_max_angle }}"), String(Servo::MAX_ANGLE));
 
             content.replace(
-                F("{{ invalid_angle_error }}"),
-                invalidAngle ? F("<p>ERROR: INVALID ANGLE!</p>") : F("")
+                PSTR("{{ invalid_angle_error }}"),
+                invalidAngle ? PSTR("<p>ERROR: INVALID ANGLE!</p>") : PSTR("")
             );
 
-            content.replace(F("{{ old_or_current }}"), newAngle ? F("Old") : F("Current"));
-            content.replace(F("{{ old_angle }}"), String(oldAngle));
+            content.replace(PSTR("{{ old_or_current }}"), newAngle ? PSTR("Old") : PSTR("Current"));
+            content.replace(PSTR("{{ old_angle }}"), String(oldAngle));
 
             content.replace(
-                F("{{ new_angle }}"),
+                PSTR("{{ new_angle }}"),
                 newAngle ? format(PSTR("<p>New angle: %d</p>"), newAngle.value()).get() : ""
             );
 
             content.replace(
-                F("{{ angle_form_value }}"),
+                PSTR("{{ angle_form_value }}"),
                 String(newAngle ? newAngle.value() : oldAngle)
             );
 

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/WiFi/LoopAwareSignalStrengthLogger.h
@@ -37,16 +37,16 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::WiFi {
         Logger& logger;
 
         void log() {
-            String message = F("WiFi ");
+            String message = PSTR("WiFi ");
 
             if (esp8266WiFi.isConnected()) {
-                message += F("signal strength: ");
+                message += PSTR("signal strength: ");
                 message += esp8266WiFi.RSSI();
-                message += F(" dBm");
+                message += PSTR(" dBm");
 
                 logger.debug(message);
             } else {
-                message += F("is NOT connected!");
+                message += PSTR("is NOT connected!");
 
                 logger.error(message);
             }

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/WiFi/WiFi.h
@@ -67,18 +67,13 @@ namespace DnWiFiDoorLock::Arduino::Esp82666::WiFi {
             waitForConnection();
 
             logger.info(format(
-                // todo: why does PSTR work here,
-                //       even though I don't use any special FLASH methods in the format() function?
-                //       * https://forum.arduino.cc/t/esp8266-f-macro-is-unnecessary-on-esp-chips/1075805/1
-                //       * https://community.platformio.org/t/esp8266-arduino-f-macro-is-unnecessary-on-esp-chips/31399
-                //       * https://discord.com/channels/583251190591258624/1062392340783829063/1062392340783829063
                 PSTR("Connected, IP address: %s"),
                 esp8266WiFi.localIP().toString().c_str()
             ));
         }
 
         void waitForConnection() {
-            String connectingMessage{F("Connecting")};
+            String connectingMessage{PSTR("Connecting")};
 
             logger.info(connectingMessage.c_str());
 
@@ -94,7 +89,7 @@ namespace DnWiFiDoorLock::Arduino::Esp82666::WiFi {
 
                 hardware.pause(500);
 
-                connectingMessage += F(".");
+                connectingMessage += PSTR(".");
 
                 logger.info(connectingMessage.c_str());
 

commit 74579a320158e46781917a2952b51a84a5727159

FurnaceController: bugfix: PSTR() -> back to F()
I don't why: PSTR() does not work, while F() does…

Problem introduced in 894b03d95838f30e056df076c08303acb42bd12e

lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/FurnaceController.h
@@ -99,9 +99,10 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         ) {
             logger.info(PSTR("apiPostAction()"));
 
-            if (body.equals(PSTR("ON"))) {
+            // todo: why PSTR() does NOT work here?
+            if (body.equals(F("ON"))) {
                 furnace.turnOnHeater();
-            } else if (body.equals(PSTR("OFF"))) {
+            } else if (body.equals(F("OFF"))) {
                 furnace.turnOffHeater();
             } else {
                 logger.warning(PSTR("apiPostAction(): Invalid value response"));