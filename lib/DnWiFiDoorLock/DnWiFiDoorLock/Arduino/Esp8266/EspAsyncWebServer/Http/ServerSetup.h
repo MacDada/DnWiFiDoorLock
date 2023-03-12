baf5606b54f70be084cd2fb63a2d17b10c67dd08
MacDada <code@dnowak.pl>
2023-03-12 04:55:18 +0100

    ServerSetup: naming routes

--- lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/ServerSetup.h
+++ lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/ServerSetup.h
@@ -98,69 +98,148 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         }
 
         void setupRouting() {
-            registerRoute(PSTR("/"), HTTP_GET, [] (AsyncWebServerRequest& request) {
-                request.redirect(PSTR("/furnace"));
-            });
+            registerRoute(
+                PSTR("app.index"),
+                PSTR("/"),
+                HTTP_GET,
+                [] (AsyncWebServerRequest& request) {
+                    request.redirect(PSTR("/furnace"));
+                }
+            );
 
-            registerRoute(PSTR("/doorlock"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                doorLockController.statusAction(request);
-            });
+            registerRoute(
+                PSTR("app.doorlock.index"),
+                PSTR("/doorlock"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    doorLockController.statusAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/doorlock/switch"), HTTP_POST, [&] (AsyncWebServerRequest& request) {
-                doorLockController.switchAction(request);
-            });
+            registerRoute(
+                PSTR("app.doorlock.switch"),
+                PSTR("/doorlock/switch"),
+                HTTP_POST,
+                [&] (AsyncWebServerRequest& request) {
+                    doorLockController.switchAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/furnace"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                furnaceController.statusAction(request);
-            });
+            registerRoute(
+                PSTR("app.furnace.index"),
+                PSTR("/furnace"), HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    furnaceController.statusAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/furnace/switch"), HTTP_POST, [&] (AsyncWebServerRequest& request) {
-                furnaceController.switchAction(request);
-            });
+            registerRoute(
+                PSTR("app.furnace.switch"),
+                PSTR("/furnace/switch"),
+                HTTP_POST,
+                [&] (AsyncWebServerRequest& request) {
+                    furnaceController.switchAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/api/furnace"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                furnaceController.apiGetAction(request);
-            });
+            registerRoute(
+                PSTR("app.furnace.api.get"),
+                PSTR("/api/furnace"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    furnaceController.apiGetAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/api/furnace"), HTTP_POST, [&] (AsyncWebServerRequest& request, const String& body) {
-                furnaceController.apiPostAction(request, body);
-            });
+            registerRoute(
+                PSTR("app.furnace.api.post"),
+                PSTR("/api/furnace"),
+                HTTP_POST,
+                [&] (AsyncWebServerRequest& request, const String& body) {
+                    furnaceController.apiPostAction(request, body);
+                }
+            );
 
-            registerRoute(PSTR("/modal.css"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.modalCssAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.modal.css"),
+                PSTR("/modal.css"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.modalCssAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/modal.js"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.modalJsAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.modal.js"),
+                PSTR("/modal.js"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.modalJsAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/ajax-form.js"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.ajaxFormJsAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.ajax_form.js"),
+                PSTR("/ajax-form.js"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.ajaxFormJsAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/input-number-buttons.js"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.inputNumberButtonsJsAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.input_number_buttons.js"),
+                PSTR("/input-number-buttons.js"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.inputNumberButtonsJsAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/servo-button.js"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.javascriptAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.servo_button.js"),
+                PSTR("/servo-button.js"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.javascriptAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/servo-button.css"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.cssAction(request);
-            });
+            registerRoute(
+                PSTR("app.assets.servo_button.css"),
+                PSTR("/servo-button.css"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.cssAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/servo-button"), HTTP_POST, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.updateSettingsAction(request);
-            });
+            registerRoute(
+                PSTR("app.servo_button.update"),
+                PSTR("/servo-button"),
+                HTTP_POST,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.updateSettingsAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/servo-button"), HTTP_GET, [&] (AsyncWebServerRequest& request) {
-                servoButtonController.showSettingsAction(request);
-            });
+            registerRoute(
+                PSTR("app.servo_button.show"),
+                PSTR("/servo-button"),
+                HTTP_GET,
+                [&] (AsyncWebServerRequest& request) {
+                    servoButtonController.showSettingsAction(request);
+                }
+            );
 
-            registerRoute(PSTR("/servo"), HTTP_GET | HTTP_POST, [&] (AsyncWebServerRequest& request) {
-                servoController.angleAction(request);
-            });
+            registerRoute(
+                PSTR("app.servo.angle"),
+                PSTR("/servo"),
+                HTTP_GET | HTTP_POST,
+                [&] (AsyncWebServerRequest& request) {
+                    servoController.angleAction(request);
+                }
+            );
 
             server.onNotFound([&] (AsyncWebServerRequest* const request) {
                 assertRequestWasGiven(request);
@@ -179,20 +258,22 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
         )>;
 
         void registerRoute(
+            const char* const name,
             const char* const uri,
             const WebRequestMethodComposite method,
             OnRequestCallback onRequest
         ) {
-            server.on(uri, method, [&, onRequest = std::move(onRequest)] (
+            server.on(uri, method, [&, name, onRequest = std::move(onRequest)] (
                 AsyncWebServerRequest* const request
             ) {
-                onMatchedRoute(request);
+                onMatchedRoute(name, request);
 
                 onRequest(*request);
             });
         }
 
         void registerRoute(
+            const char* const name,
             const char* const uri,
             const WebRequestMethodComposite method,
             OnRequestBodyCallback onRequestBody
@@ -200,36 +281,41 @@ namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
             server.on(uri, method, [] (const AsyncWebServerRequest* const) {
                 // cannot be nullptr or the route is not found o.O
                 // ignore it: we use the callback with request body
-            }, nullptr, [&, onRequestBody = std::move(onRequestBody)] (
+            }, nullptr, [&, name, onRequestBody = std::move(onRequestBody)] (
                 AsyncWebServerRequest* const request,
                 const uint8_t* bodyData,
                 const size_t bodyDataLength,
                 const size_t index,
                 const size_t total
             ) {
-                onMatchedRoute(request);
+                onMatchedRoute(name, request);
 
                 onRequestBody(*request, dataToString(bodyData, bodyDataLength));
             });
         }
 
-        void onMatchedRoute(AsyncWebServerRequest* const request) {
+        void onMatchedRoute(
+            const char* const routeName,
+            AsyncWebServerRequest* const request
+        ) {
             assertRequestWasGiven(request);
 
-            const auto method = request->methodToString();
-            const auto url = request->url().c_str();
+            const auto requestMethod = request->methodToString();
+            const auto requestUrl = request->url().c_str();
 
             logger.debug(format(
-                PSTR("HttpServer: matched route: %s %s"),
-                method,
-                url
+                PSTR("HttpServer: matched route: %s: %s %s"),
+                routeName,
+                requestMethod,
+                requestUrl
             ));
 
-            request->onDisconnect([&, method, url] {
+            request->onDisconnect([&, routeName, requestMethod, requestUrl] {
                 logger.debug(format(
-                    PSTR("HttpServer: disconnected client: %s %s"),
-                    method,
-                    url
+                    PSTR("HttpServer: disconnected client: %s: %s %s"),
+                    routeName,
+                    requestMethod,
+                    requestUrl
                 ));
             });
         }
