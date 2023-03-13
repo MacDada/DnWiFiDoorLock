1b53faef6e63067533fd2854024e29e94a569467
MacDada <code@dnowak.pl>
2023-03-13 01:28:35 +0100

    codestyle

--- lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/Controller.h
+++ lib/DnWiFiDoorLock/DnWiFiDoorLock/Arduino/Esp8266/EspAsyncWebServer/Http/Controller.h
@@ -8,26 +8,56 @@
 namespace DnWiFiDoorLock::Arduino::Esp8266::EspAsyncWebServer::Http {
     class Controller {
     protected:
-        static const int HTTP_RESPONSE_STATUS_OK = 200;
+        static
+        const
+        int HTTP_RESPONSE_STATUS_OK = 200;
 
-        static const int HTTP_RESPONSE_STATUS_ACCEPTED = 202;
+        static
+        const
+        int HTTP_RESPONSE_STATUS_ACCEPTED = 202;
 
-        static const int HTTP_RESPONSE_STATUS_REDIRECT = 303;
+        static
+        const
+        int HTTP_RESPONSE_STATUS_REDIRECT = 303;
 
-        static const int HTTP_RESPONSE_STATUS_BAD_REQUEST = 400;
+        static
+        const
+        int HTTP_RESPONSE_STATUS_BAD_REQUEST = 400;
 
-        static const int HTTP_RESPONSE_STATUS_METHOD_NOT_ALLOWED = 405;
+        static
+        const
+        int HTTP_RESPONSE_STATUS_METHOD_NOT_ALLOWED = 405;
 
-        static PROGMEM constexpr const char HTTP_RESPONSE_CONTENT_TYPE_PLAIN[] = "text/plain";
+        static
+        PROGMEM
+        constexpr
+        const
+        char HTTP_RESPONSE_CONTENT_TYPE_PLAIN[] = "text/plain";
 
-        static PROGMEM constexpr const char HTTP_RESPONSE_CONTENT_TYPE_HTML[] = "text/html";
+        static
+        PROGMEM
+        constexpr
+        const
+        char HTTP_RESPONSE_CONTENT_TYPE_HTML[] = "text/html";
 
-        static PROGMEM constexpr const char HTTP_RESPONSE_CONTENT_TYPE_JSON[] = "application/json";
+        static
+        PROGMEM
+        constexpr
+        const
+        char HTTP_RESPONSE_CONTENT_TYPE_JSON[] = "application/json";
 
-        static PROGMEM constexpr const char HTTP_RESPONSE_CONTENT_TYPE_CSS[] = "text/css";
+        static
+        PROGMEM
+        constexpr
+        const
+        char HTTP_RESPONSE_CONTENT_TYPE_CSS[] = "text/css";
 
         // https://stackoverflow.com/a/21098951/666907
-        static PROGMEM constexpr const char HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT[] = "text/javascript";
+        static
+        PROGMEM
+        constexpr
+        const
+        char HTTP_RESPONSE_CONTENT_TYPE_JAVASCRIPT[] = "text/javascript";
 
         bool isRequestMethodPost(
             const AsyncWebServerRequest& request
